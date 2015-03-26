3f38bfcad4c52004d97b50a7f5a99d175715e53e
Md Aman Ullah <amanu092@gmail.com>
2015-03-26 22:18:09 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,15 +3,16 @@
 
 $a= 'hello';
 $b= 'okay!';
-//print 2 variables 
+//print the variables 
+
 echo 'a = '.$a.' '.'b= '.$b; // a= 10 b= 34
-//operate bitwise xor operataion between the 2 varables
+//operate bitwise xor operataion between the varables
 
 $a ^= $b;
 $b ^= $a;
 $a ^= $b;
 
 echo '</br>';
-//now print the variables again and ntice that the values are cuccessfully swapped
+//now print the variables again and notice that the values are cuccessfully swapped
 echo 'a = '.$a.' '.'b= '.$b; // a= 34 b = 10 
 ?>
